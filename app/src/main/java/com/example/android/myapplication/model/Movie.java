21070185fa25ef3305901c625386f1c59516a69a
Ghenamd <evaflorn@yahoo.com>
2018-02-24 10:34:55 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/myapplication/model/Movie.java
@@ -0,0 +1,58 @@
+package com.example.android.myapplication.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Ghena on 24/02/2018.
+ */
+
+public class Movie {
+
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("results")
+    @Expose
+    private List<MovieResult> results = null;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public List<MovieResult> getResults() {
+        return results;
+    }
+
+    public void setResults(List<MovieResult> results) {
+        this.results = results;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+}
---
+++ app/src/main/java/com/example/android/myapplication/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.android.myapplication.utils;
+
+/**
+ * Created by Ghena on 24/02/2018.
+ */
+
+public class Constants {
+}
