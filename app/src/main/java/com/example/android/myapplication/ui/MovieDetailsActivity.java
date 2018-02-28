9ee6b24721fcd9fd797ef733b202cde331796529
Ghenamd <evaflorn@yahoo.com>
2018-02-28 12:57:39 +0000

    Added favourite button and rating bar changes

--- app/src/main/java/com/example/android/myapplication/ui/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/myapplication/ui/MovieDetailsActivity.java
@@ -1,8 +1,6 @@
 package com.example.android.myapplication.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +9,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.android.myapplication.R;
 import com.example.android.myapplication.model.MovieResult;
@@ -29,25 +28,21 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView date;
     @BindView(R.id.overview)
     TextView overview;
+    @BindView(R.id.image_button)
+    ToggleButton mButton;
     private RatingBar mRatingBar;
     private static final String TAG = "MovieDetailsActivity";
+    private boolean isClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
         ButterKnife.bind(this);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         mRatingBar = findViewById(R.id.ratingBar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setFavouriteButton();
         populateUi();
     }
 
@@ -59,14 +54,17 @@ public class MovieDetailsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
     //Method to set the average rating for a specific Movie
-    private void setRatingBar(double d){
-        double half = d/2;
-        if(half>0){mRatingBar.setRating(Float.parseFloat(String.valueOf(half)));}
+    private void setRatingBar(double d) {
+        double half = d / 2;
+        if (half > 0) {
+            mRatingBar.setRating(Float.parseFloat(String.valueOf(half)));
+        }
 
     }
 
-    private void populateUi(){
+    private void populateUi() {
         MovieResult movieResult = getIntent().getParcelableExtra(Constants.PARCEL);
         title.setText(movieResult.getOriginalTitle());
         Picasso.with(this).load(Constants.IMAGE_BASE_URL + movieResult.getPosterPath())
@@ -74,5 +72,21 @@ public class MovieDetailsActivity extends AppCompatActivity {
         date.setText(movieResult.getReleaseDate());
         overview.setText(movieResult.getOverview());
         setRatingBar(movieResult.getVoteAverage());
+    }
+
+    private void setFavouriteButton() {
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mButton.isChecked()) {
+                    mButton.setBackgroundResource(R.drawable.favourite_red);
+                } else {
+                    mButton.setBackgroundResource(R.drawable.favourite_white);
+                }
+
+            }
+        });
+
+
     }
 }
